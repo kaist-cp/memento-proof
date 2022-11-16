acc151b3ee4273046e76736e27ef63b81b2c1dfe
Kyeongmin Cho <kyeongmin.cho@kaist.ac.kr>
2022-11-17 00:42:28 +0900

    proof: Fix DR
    Signed-off-by: Kyeongmin Cho <kyeongmin.cho@kaist.ac.kr>

--- src/proof/DR.v
+++ src/proof/DR.v
@@ -12,13 +12,11 @@ From Memento Require Import Common.
 Set Implicit Arguments.
 
 Definition DR (env: Env.t) (s: list Stmt) :=
-  forall tr tr' thr thr' thr_term thr_term' ts mmts,
-    thr = Thread.mk s [] ts mmts ->
-    thr' = Thread.mk s [] ts thr_term.(Thread.mmts) ->
-    Thread.rtc env tr thr thr_term [] ->
-    Thread.rtc env tr' thr' thr_term' [] ->
+  forall tr tr' thr_term thr_term' ts mmts,
+    Thread.rtc env tr (Thread.mk s [] ts mmts) thr_term [] ->
+    Thread.rtc env tr' (Thread.mk s [] ts thr_term.(Thread.mmts)) thr_term' [] ->
   exists tr_x s_x c_x ts_x,
-    Thread.rtc env tr_x thr (Thread.mk s_x c_x ts_x thr_term'.(Thread.mmts)) []
+    Thread.rtc env tr_x (Thread.mk s [] ts mmts) (Thread.mk s_x c_x ts_x thr_term'.(Thread.mmts)) []
     /\ tr_x ~ tr ++ tr'
     /\ (STOP thr_term.(Thread.stmt) thr_term.(Thread.cont) ->
           thr_term.(Thread.stmt) = s_x
@@ -40,8 +38,7 @@ Lemma DR_RW :
   DR env s.
 Proof.
   intros env envt labs s TYPEJ ENVTJ.
-  intros tr tr' thr thr' thr_term thr_term' ts mmts THR THR' EX1 EX2.
-  induction ENVTJ.
+  induction ENVTJ; intros tr tr' thr_term thr_term' ts mmts EX1 EX2.
   - eexists []. eexists s. eexists []. eexists ts.
     inv EX1; ss; cycle 1.
     { inv ONE. inv NORMAL_STEP; inv STEP; ss. }
