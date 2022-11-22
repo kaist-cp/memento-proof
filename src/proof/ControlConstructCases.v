6e37d4610da95a92c8856bb9704429822d132d01
Kyeongmin Cho <kyeongmin.cho@kaist.ac.kr>
2022-11-22 21:35:38 +0900

    proof: Add loop lemmas
    Signed-off-by: Kyeongmin Cho <kyeongmin.cho@kaist.ac.kr>

--- src/proof/ControlConstructCases.v
+++ src/proof/ControlConstructCases.v
@@ -11,7 +11,7 @@ From Memento Require Import Common.
 
 Set Implicit Arguments.
 
-Lemma chkpt_fn_cases :
+Lemma chkpt_fn_cases:
   forall env c_base tr thr thr_term c c' rmap r,
     c_base = [] ->
     Thread.rtc env tr thr thr_term c_base ->
@@ -209,7 +209,7 @@ Proof.
         rewrite <- app_inv_tail_iff. rewrite <- app_assoc. eauto.
 Qed.
 
-Lemma loop_cases :
+Lemma loop_cases:
   forall env c_base tr thr thr_term c c' rmap r s,
     c_base = [] ->
     Thread.rtc env tr thr thr_term c_base ->
@@ -218,14 +218,14 @@ Lemma loop_cases :
   <<LOOP_ONGOING: Thread.rtc env tr thr thr_term [c']>>
   \/
   <<LOOP_DONE:
-    exists s_r ts_r,
-      Thread.rtc env tr
-        (Thread.mk thr.(Thread.stmt) thr.(Thread.cont) thr.(Thread.ts) thr.(Thread.mmts))
-        (Thread.mk (stmt_break :: s_r) [c'] ts_r thr_term.(Thread.mmts))
-        [c']
-      /\ thr_term.(Thread.stmt) = []
-      /\ thr_term.(Thread.cont) = []
-      /\ thr_term.(Thread.ts) = TState.mk rmap ts_r.(TState.time)>>.
+      exists s_r ts_r,
+        Thread.rtc env tr
+          thr
+          (Thread.mk (stmt_break :: s_r) [c'] ts_r thr_term.(Thread.mmts))
+          [c']
+        /\ thr_term.(Thread.stmt) = []
+        /\ thr_term.(Thread.cont) = []
+        /\ thr_term.(Thread.ts) = TState.mk rmap ts_r.(TState.time)>>.
 Proof.
   intros env c_base tr thr thr_term c c' rmap r s EMPTY RTC.
   generalize dependent rmap. generalize dependent r. generalize dependent s. generalize dependent c. generalize dependent c'.
@@ -413,4 +413,61 @@ Proof.
       * right. esplits; eauto.
         econs 2; eauto; [| rewrite app_nil_l]; ss.
         econs; ss. econs 15. econs; eauto; ss.
+Qed.
+
+Lemma first_loop_iter:
+  forall env tr thr thr_term c c' rmap r s,
+    Thread.rtc env tr thr thr_term [c'] ->
+    thr.(Thread.cont) = c ++ [c'] ->
+    c' = Cont.loopcont rmap r s [] ->
+  <<FIRST_ONGOING:
+      exists c_pfx, thr_term.(Thread.cont) = c_pfx ++ [c']
+        /\ Thread.rtc env tr
+            thr
+            (Thread.mk thr_term.(Thread.stmt) c_pfx thr_term.(Thread.ts) thr_term.(Thread.mmts))
+            []>>
+  \/
+  <<FIRST_DONE:
+    exists tr1 tr2 e s1 ts1 mmts1,
+      tr = tr1 ++ tr2
+      /\ Thread.rtc env tr1
+          (Thread.mk thr.(Thread.stmt) c thr.(Thread.ts) thr.(Thread.mmts))
+          (Thread.mk ((stmt_continue e) :: s1) [] ts1 mmts1)
+          []
+      /\ Thread.rtc env tr2
+          (Thread.mk ((stmt_continue e) :: s1) [c'] ts1 mmts1)
+          thr_term
+          [c']>>.
+Proof.
+  admit.
+Qed.
+
+Lemma last_loop_iter:
+  forall env tr thr thr_term c c' rmap r s,
+    Thread.rtc env tr thr thr_term [c'] ->
+    thr.(Thread.cont) = c ++ [c'] ->
+    c' = Cont.loopcont rmap r s [] ->
+  exists tr1 tr2 s1 c1 ts1 mmts1 c_pfx,
+    tr = tr1 ++ tr2
+    /\ (<<LAST_FIRST:
+          s1 = thr.(Thread.stmt) /\ c1 = c
+          /\ ts1 = thr.(Thread.ts) /\ mmts1 = thr.(Thread.mmts)
+          /\ tr1 = []>>
+       \/
+       <<LAST_CONT:
+          exists e s_r ts_r,
+            Thread.rtc env tr1
+              thr
+              (Thread.mk ((stmt_continue e) :: s_r) [c'] ts_r mmts1)
+              [c']
+            /\ s1 = s
+            /\ c1 = []
+            /\ ts1 = TState.mk (VRegMap.add r (sem_expr ts_r.(TState.regs) e) rmap) ts_r.(TState.time)>>)
+    /\ thr_term.(Thread.cont) = c_pfx ++ [c']
+    /\ Thread.rtc env tr2
+          (Thread.mk s1 c1 ts1 mmts1)
+          (Thread.mk thr_term.(Thread.stmt) c_pfx thr_term.(Thread.ts) thr_term.(Thread.mmts))
+          [].
+Proof.
+  admit.
 Qed.
